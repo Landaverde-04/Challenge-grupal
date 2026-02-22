db5e878ada92eaca605fccc0c5e37f718dd08c33
Landaverde-04 <landaverde.kevin04@gmail.com>
2026-02-21 20:46:44 -0600

    Primer commit

---
+++ domain/models.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+
+class Usuario:
+    def __init__(self, id, nombres, apellidos, dui, pin, rol, username=None):
+        self.id = id
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.dui = dui
+        self.pin = pin
+        self.rol = rol
+        
+        if self.rol == 'Administrador':
+            if username:
+                self.username = username
+            else:
+                self.username = self._generar_username_admin()
+
+        else:
+            self.username = ""
+
+    def _generar_username_admin(self):
+        primer_nombre = self.nombres.split()[0] 
+        primer_apellido = self.apellidos.split()[0]
+        siglas = primer_nombre[0] + primer_apellido[0]
+        return f"{siglas.upper()}{self.id}"
+        
+    def to_dict(self):
+        return {
+                'id': self.id,
+                'nombres': self.nombres,
+                'apellidos': self.apellidos,
+                'dui': self.dui,
+                'pin': self.pin,
+                'rol': self.rol,
+                'username': self.username
+            }
+    
+class Cuenta:
+    def __init__(self, id_cuenta, propietario_id, tipo, saldo=0.0, estado="Activa"):
+        self.id_cuenta = id_cuenta
+        self.propietario_id = propietario_id # Relación con el ID del Usuario [cite: 163]
+        self.tipo = tipo # "Ahorro" o "Corriente" 
+        self.saldo = float(saldo)
+        self.estado = estado # "Activa" o "Bloqueada" 
+
+    def to_dict(self):
+        return {
+            'id_cuenta': self.id_cuenta,
+            'propietario_id': self.propietario_id,
+            'tipo': self.tipo,
+            'saldo': self.saldo,
+            'estado': self.estado
+        }
---
+++ infrastructure/file_manager.py
@@ -0,0 +1,52 @@
+import csv
+import os
+
+class FileManager:
+    def __init__(self, archivo_nombre, fieldnames):
+        self.archivo_path = f"data/{archivo_nombre}"
+        self.fieldnames = fieldnames
+        self._inicializar_archivo()
+
+    def _inicializar_archivo(self):
+        # Verificar si el directorio "data" existe, si no, crearlo
+        if not os.path.exists("data"):
+            os.makedirs("data")
+        # Verificar si el archivo existe, si no, crearlo con los encabezados
+        if not os.path.exists(self.archivo_path):
+            with open(self.archivo_path, mode='w', newline='', encoding='utf-8') as file:
+                writer = csv.DictWriter(file, fieldnames=self.fieldnames)
+                writer.writeheader()
+
+    #Metodo para leer los datos del archivo CSV y devolverlos como una lista de diccionarios
+    def leer_todos(self):
+        #Lista vacia para almacenar los datos leidos del archivo CSV
+        data =  []
+        if os.path.exists(self.archivo_path):
+            with open(self.archivo_path, mode='r', newline='', encoding='utf-8') as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    data.append(row)
+        return data
+    
+    #Metodo para escribir un nuevo registro en el archivo CSV
+    def agregar(self, registro_dict): #aca le paso un diccionario con los datos del nuevo registro
+        with open(self.archivo_path, mode='a', newline='', encoding='utf-8') as file:
+            #En este caso usamos el modo 'a' para abrir el archivo usando el modo append, lo que significa que los nuevos registros se agregarán al final del archivo sin sobrescribir los datos existentes. 
+            writer = csv.DictWriter(file, fieldnames=self.fieldnames)
+            writer.writerow(registro_dict)
+
+    #metodo para poder guardar todo el diccionario de 0
+    def guardar_todo(self, lista_registros):
+        with open(self.archivo_path, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=self.fieldnames)
+            writer.writeheader() #Escribimos el encabezado antes de escribir los registros
+            writer.writerows(lista_registros) #Escribimos todos los registros de la lista en el archivo CSV
+
+    #metodo para obtener el ultimo id nuevo que se hara
+    def obtener_nuevo_id(self):
+        registros = self.leer_todos()
+        if not registros:
+            return 1
+        else:
+            ultimo_id = registros[-1]
+            return int(ultimo_id['id']) + 1
