bf08c4a3c032f79ce39e81888bed255a45e1584c
Joaquín Leonor <joachimleonor@gmail.com>
2026-02-22 18:00:15 -0600

    feat: cargar transacciones con numpy en AnalyticsService

--- application/analytics_service.py
+++ application/analytics_service.py
@@ -1,3 +1,35 @@
+import numpy as np
+import os
+
 class AnalyticsService:
+
     def __init__(self):
-        print("AnalyticsService inicializado correctamente.")
+        self.path_transacciones = os.path.join("data", "transacciones.csv")
+        self._cargar_datos()
+        print("Datos cargados correctamente para análisis.")
+
+    def _cargar_datos(self):
+
+        if not os.path.exists(self.path_transacciones):
+            print("No existe archivo de transacciones.")
+            self.transacciones = np.array([])
+            return
+
+        self.transacciones = np.genfromtxt(
+            self.path_transacciones,
+            delimiter=",",
+            skip_header=1,
+            dtype=None,
+            encoding="utf-8"
+        )
+
+        if self.transacciones.size > 0:
+            self.id_cuenta = self.transacciones['f1']
+            self.tipo = self.transacciones['f2']
+            self.monto = self.transacciones['f3']
+            self.fecha_str = self.transacciones['f4']
+
+            self.fechas = self.fecha_str.astype('datetime64[s]')
+            self.dias = self.fechas.astype('datetime64[D]')
+        else:
+            print("No hay transacciones registradas.")
